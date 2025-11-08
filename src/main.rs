c1432a39ee31e753f651cc8b587fcd9b0549ec74
JohnTheCoolingFan <ivan8215145640@gmail.com>
2025-11-08 18:43:53 +0300

    General improvements

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::{collections::HashMap, fs::File, io::Write, process::exit, time::Instant};
+use std::{collections::HashMap, fmt::Display, fs::File, io::Write, path::PathBuf, time::Instant};
 
-use image::{
-    GenericImage, GenericImageView, ImageBuffer, ImageReader, Pixel, Rgb, RgbImage, SubImage,
-};
+use image::{GenericImage, ImageBuffer, Rgb};
 use indicatif::ProgressBar;
 use rayon::prelude::*;
+
+mod quadtree;
+use quadtree::*;
 
 const BLACK: Rgb<u8> = Rgb([0, 0, 0]);
 const WHITE: Rgb<u8> = Rgb([253, 255, 255]);
@@ -17,175 +18,82 @@ const HEIGHT_ORIGINAL: u32 = 360;
 const WIDTH: u32 = WIDTH_ORIGINAL / DOWNSCALE as u32;
 const HEIGHT: u32 = HEIGHT_ORIGINAL / DOWNSCALE as u32;
 
-const SAMPLE_SIZE: u32 = 4;
+const SAMPLE_SIZE: u32 = 8;
 const CHUNKS_X: u32 = WIDTH / SAMPLE_SIZE;
 const CHUNKS_Y: u32 = HEIGHT / SAMPLE_SIZE;
 
 const CANVAS_SIZE: u32 = 512 / (DOWNSCALE as u32);
 
+type TileData = [u8; ((SAMPLE_SIZE * SAMPLE_SIZE) as usize).div_ceil(8)];
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct TileId(TileData);
+
+impl TileId {
+    fn from_samples(mut samples: [bool; (SAMPLE_SIZE * SAMPLE_SIZE) as usize]) -> Self {
+        let mut res = TileData::default();
+        samples.reverse();
+        let (full, remainder) = samples.as_chunks::<8>();
+        for (i, chunk) in full.iter().enumerate() {
+            let val = chunk
+                .iter()
+                .map(|v| if *v { 1 } else { 0 })
+                .fold(0, |acc, v| (acc << 1) | v);
+            res[i] = val;
+        }
+        if !remainder.is_empty() {
+            let mut remainder_buf = [false; 8];
+            remainder_buf[0..(remainder.len())].copy_from_slice(remainder);
+            let val = remainder_buf
+                .iter()
+                .map(|v| if *v { 1 } else { 0 })
+                .fold(0, |acc, v| (acc << 1) | v);
+            res[res.len() - 1] = val;
+        }
+
+        Self(res)
+    }
+}
+
+impl Display for TileId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for byte in self.0 {
+            write!(f, "{byte:02x}")?;
+        }
+        Ok(())
+    }
+}
+
 fn encode_pixels_hex(values: [bool; (SAMPLE_SIZE * SAMPLE_SIZE) as usize]) -> String {
-    let mut res = String::with_capacity(values.len() / 4);
-
-    for chunk in values.as_chunks::<8>().0.iter().rev() {
-        let val: u8 = chunk
-            .iter()
-            .rev()
-            .map(|v| if *v { 1 } else { 0 })
-            .fold(0, |acc, v| (acc << 1) | v);
-
-        res.push_str(&format!("{val:02x}"));
-    }
-
-    res
-}
-
-#[derive(Debug)]
-struct QuadTree {
-    root: QuadTreeNode,
-}
-
-impl QuadTree {
-    fn build(image: &RgbImage) -> Self {
-        QuadTree {
-            root: QuadTreeNode::node_from_view(
-                &image.view(0, 0, CANVAS_SIZE, CANVAS_SIZE),
-                CANVAS_SIZE,
-            ),
-        }
-    }
-
-    fn reconstruct_img(&self) -> RgbImage {
-        let mut res = RgbImage::new(CANVAS_SIZE, CANVAS_SIZE);
-        Self::reconstruct_region(&self.root, &mut res, 0, 0, CANVAS_SIZE);
-        res
-    }
-
-    fn reconstruct_region(
-        node: &QuadTreeNode,
-        img: &mut RgbImage,
-        anchor_x: u32,
-        anchor_y: u32,
-        side_size: u32,
-    ) {
-        match node {
-            // image empty by default
-            QuadTreeNode::Empty => {}
-            QuadTreeNode::Full => {
-                for y in 0..side_size {
-                    for x in 0..side_size {
-                        img.put_pixel(x + anchor_x, y + anchor_y, WHITE);
-                    }
-                }
-            }
-            QuadTreeNode::Shaped(tile_id) => {
-                let masknum = u64::from_str_radix(tile_id, 16).unwrap();
-                for i in 0..(side_size * side_size) {
-                    let x = i % side_size;
-                    let y = i / side_size;
-                    let pix_value = (masknum >> i) & 1;
-                    if pix_value == 1 {
-                        img.put_pixel(x + anchor_x, y + anchor_y, WHITE);
-                    } else {
-                        img.put_pixel(x + anchor_x, y + anchor_y, BLACK);
-                    }
-                }
-            }
-            QuadTreeNode::Subdivided(children) => {
-                for (child, (x, y)) in children.iter().zip([(0, 0), (1, 0), (0, 1), (1, 1)]) {
-                    let new_side_size = side_size / 2;
-                    let new_anchor_x = anchor_x + (new_side_size * x);
-                    let new_anchor_y = anchor_y + (new_side_size * y);
-                    Self::reconstruct_region(child, img, new_anchor_x, new_anchor_y, new_side_size);
-                }
-            }
-        }
-    }
-}
-
-#[derive(Debug)]
-enum QuadTreeNode {
-    Full,
-    Empty,
-    Subdivided(Box<[QuadTreeNode; 4]>),
-    Shaped(String),
-}
-
-impl QuadTreeNode {
-    fn all_white<I: GenericImageView<Pixel = Rgb<u8>>>(img: &SubImage<&I>, side_size: u32) -> bool {
-        (0..side_size)
-            .flat_map(|y| (0..side_size).map(move |x| img.get_pixel(x, y)))
-            .all(|v| v != BLACK)
-    }
-
-    fn all_black<I: GenericImageView<Pixel = Rgb<u8>>>(img: &SubImage<&I>, side_size: u32) -> bool {
-        !(0..side_size)
-            .flat_map(|y| (0..side_size).map(move |x| img.get_pixel(x, y)))
-            .any(|v| v != BLACK)
-    }
-
-    fn node_from_view<I: GenericImageView<Pixel = Rgb<u8>>>(
-        view: &SubImage<&I>,
-        side_size: u32,
-    ) -> Self {
-        if Self::all_white(view, side_size) {
-            Self::Full
-        } else if Self::all_black(view, side_size) {
-            Self::Empty
-        } else if side_size == SAMPLE_SIZE {
-            let mut bools = [false; (SAMPLE_SIZE * SAMPLE_SIZE) as usize];
-            for x in 0..SAMPLE_SIZE {
-                for y in 0..SAMPLE_SIZE {
-                    bools[(x + y * SAMPLE_SIZE) as usize] = view.get_pixel(x, y) != BLACK
-                }
-            }
-            Self::Shaped(encode_pixels_hex(bools))
-        } else {
-            let coords = [(0, 0), (1, 0), (0, 1), (1, 1)];
-            let subdivisions = coords.map(|(x, y)| {
-                let new_side_size = side_size / 2;
-                let new_view = view.view(
-                    x * new_side_size,
-                    y * new_side_size,
-                    new_side_size,
-                    new_side_size,
-                );
-                Self::node_from_view(&new_view, new_side_size)
-            });
-            Self::Subdivided(Box::new(subdivisions))
-        }
-    }
-
-    fn get_shapes(&self) -> Vec<&String> {
-        match self {
-            Self::Full | Self::Empty => vec![],
-            Self::Shaped(s) => vec![s],
-            Self::Subdivided(nodes) => nodes.iter().flat_map(|node| node.get_shapes()).collect(),
-        }
-    }
-
-    fn as_lua(&self) -> String {
-        match self {
-            Self::Empty => "0".to_string(),
-            Self::Full => "1".to_string(),
-            Self::Shaped(s) => format!("\"{s}\""),
-            Self::Subdivided(children) => {
-                let c0 = children[0].as_lua();
-                let c1 = children[1].as_lua();
-                let c2 = children[2].as_lua();
-                let c3 = children[3].as_lua();
-
-                format!("{{{c0},{c1},{c2},{c3}}}")
-            }
-        }
-    }
+    TileId::from_samples(values).to_string()
 }
 
 fn main() {
+    let start = Instant::now();
+
+    let out_dir = PathBuf::from("output");
+    if !out_dir.exists() {
+        std::fs::create_dir(&out_dir).unwrap();
+    }
+
     let progress_bar = ProgressBar::new(IMAGE_AMOUNT).with_message("Processing images");
-    let mut chunk_counts: HashMap<String, u32> = HashMap::new();
-    let mut out_file = File::create("frames-tree.lua").unwrap();
-    out_file.write_all(b"return {").unwrap();
+
+    let mut chunk_counts: HashMap<TileId, u32> = HashMap::new();
+
+    let mut tree_out_file = File::create(out_dir.join("frames-tree.lua")).unwrap();
+    tree_out_file.write_all(b"return {").unwrap();
+
+    let mut filled_squares_file = File::create(out_dir.join("filled-squares.lua")).unwrap();
+    filled_squares_file.write_all(b"return {").unwrap();
+
     let mut working_buffer = ImageBuffer::new(CANVAS_SIZE, CANVAS_SIZE);
+
+    let processing_start = Instant::now();
+    progress_bar.println(format!(
+        "Started processing at {}ms",
+        (processing_start - start).as_millis()
+    ));
+
     for inum in 0..IMAGE_AMOUNT {
         let inum = inum + 1;
         let image = image::open(format!("images/{DOWNSCALE}x/frame{inum:04}.png")).unwrap();
@@ -195,34 +103,72 @@ fn main() {
         let quad_tree = QuadTree::build(&working_buffer);
 
         for tile_id in quad_tree.root.get_shapes() {
-            *chunk_counts.entry(tile_id.clone()).or_insert(0) += 1;
+            *chunk_counts.entry(*tile_id).or_insert(0) += 1;
         }
 
         let quad_tree_lua = quad_tree.root.as_lua();
 
-        out_file.write_all(quad_tree_lua.as_bytes()).unwrap();
-        out_file.write_all(b",").unwrap();
+        tree_out_file.write_all(quad_tree_lua.as_bytes()).unwrap();
+        tree_out_file.write_all(b",").unwrap();
+
+        filled_squares_file.write_all(b"{").unwrap();
+        for (x, y, size) in quad_tree.all_filled_squares() {
+            filled_squares_file
+                .write_all(format!("{{{x},{y},{size}}},").as_bytes())
+                .unwrap();
+        }
+        filled_squares_file.write_all(b"},").unwrap();
 
         progress_bar.inc(1);
     }
-    out_file.write_all(b"}").unwrap();
-    progress_bar.finish();
-    println!("All done");
+
+    tree_out_file.write_all(b"}").unwrap();
+    filled_squares_file.write_all(b"}").unwrap();
+
+    progress_bar.finish_with_message("All done");
+
+    let processing_end = Instant::now();
+    println!(
+        "Processing ended at {}ms, took {}ms",
+        (processing_end - processing_start).as_millis(),
+        (processing_end - start).as_millis()
+    );
+
     println!("Number of unique chunks: {}", chunk_counts.len());
 
-    let mut used_numbers_file = File::create("used-tiles.lua").unwrap();
+    let used_numbers_start = Instant::now();
+
+    let mut used_numbers_file = File::create(out_dir.join("used-tiles.lua")).unwrap();
     used_numbers_file.write_all(b"return {").unwrap();
+
     for tile_id in chunk_counts.keys() {
         used_numbers_file
             .write_all(format!("\"{tile_id}\",").as_ref())
             .unwrap();
     }
+
     used_numbers_file.write_all(b"}").unwrap();
 
-    let mut out_stats = File::create(format!("tile_stats_{DOWNSCALE}x.csv")).unwrap();
+    let used_numbers_end = Instant::now();
+
+    println!(
+        "Used numbers collection took {}ms",
+        (used_numbers_end - used_numbers_start).as_millis()
+    );
+
+    let mut out_stats = File::create(out_dir.join(format!("tile_stats_{DOWNSCALE}x.csv"))).unwrap();
     writeln!(&mut out_stats, "tile_id,amount").unwrap();
+
     for (tile_id, amount) in chunk_counts.iter() {
         writeln!(&mut out_stats, "tile_{tile_id},{amount}").unwrap();
     }
+
     out_stats.flush().unwrap();
+
+    let tile_stats_end = Instant::now();
+
+    println!(
+        "Tile stats took {}ms",
+        (tile_stats_end - used_numbers_end).as_millis()
+    );
 }
---
+++ src/quadtree.rs
@@ -0,0 +1,234 @@
+use image::{GenericImageView, Rgb, RgbImage, SubImage};
+
+use crate::{BLACK, CANVAS_SIZE, SAMPLE_SIZE, TileId, WHITE, encode_pixels_hex};
+
+#[derive(Debug)]
+pub struct QuadTree {
+    pub root: QuadTreeNode,
+}
+
+impl QuadTree {
+    pub fn build(image: &RgbImage) -> Self {
+        QuadTree {
+            root: QuadTreeNode::node_from_view(
+                &image.view(0, 0, CANVAS_SIZE, CANVAS_SIZE),
+                CANVAS_SIZE,
+            ),
+        }
+    }
+
+    pub fn reconstruct_img(&self) -> RgbImage {
+        let mut res = RgbImage::new(CANVAS_SIZE, CANVAS_SIZE);
+        Self::reconstruct_region(&self.root, &mut res, 0, 0, CANVAS_SIZE);
+        res
+    }
+
+    fn reconstruct_region(
+        node: &QuadTreeNode,
+        img: &mut RgbImage,
+        anchor_x: u32,
+        anchor_y: u32,
+        side_size: u32,
+    ) {
+        match node {
+            // image empty by default
+            QuadTreeNode::Empty => {}
+            QuadTreeNode::Full => {
+                for y in 0..side_size {
+                    for x in 0..side_size {
+                        img.put_pixel(x + anchor_x, y + anchor_y, WHITE);
+                    }
+                }
+            }
+            QuadTreeNode::Shaped(tile_id) => {
+                let masknum = u64::from_str_radix(&tile_id.to_string(), 16).unwrap();
+                for i in 0..(side_size * side_size) {
+                    let x = i % side_size;
+                    let y = i / side_size;
+                    let pix_value = (masknum >> i) & 1;
+                    if pix_value == 1 {
+                        img.put_pixel(x + anchor_x, y + anchor_y, WHITE);
+                    } else {
+                        img.put_pixel(x + anchor_x, y + anchor_y, BLACK);
+                    }
+                }
+            }
+            QuadTreeNode::Subdivided(children) => {
+                for (child, (x, y)) in children.iter().zip([(0, 0), (1, 0), (0, 1), (1, 1)]) {
+                    let new_side_size = side_size / 2;
+                    let new_anchor_x = anchor_x + (new_side_size * x);
+                    let new_anchor_y = anchor_y + (new_side_size * y);
+                    Self::reconstruct_region(child, img, new_anchor_x, new_anchor_y, new_side_size);
+                }
+            }
+        }
+    }
+
+    pub fn all_filled_squares(&self) -> Vec<(u32, u32, u32)> {
+        self.iter()
+            .filter(|node| matches!(node.node, QuadTreeNode::Full))
+            .map(|node| (node.x, node.y, node.side_size(512)))
+            .collect()
+    }
+
+    pub fn iter(&self) -> QuadTreeIter<'_> {
+        QuadTreeIter {
+            top_tile_size: 512,
+            stack: vec![QuadTreeNodeWithCoords {
+                node: &self.root,
+                x: 0,
+                y: 0,
+                depth: 0,
+            }],
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct QuadTreeNodeWithCoords<'a> {
+    node: &'a QuadTreeNode,
+    x: u32,
+    y: u32,
+    depth: u8,
+}
+
+impl QuadTreeNodeWithCoords<'_> {
+    pub fn side_size(&self, top_side_size: u32) -> u32 {
+        top_side_size / (self.depth + 2) as u32
+    }
+}
+
+#[derive(Debug)]
+pub struct QuadTreeIter<'a> {
+    top_tile_size: u32,
+    stack: Vec<QuadTreeNodeWithCoords<'a>>,
+}
+
+impl<'a> Iterator for QuadTreeIter<'a> {
+    type Item = QuadTreeNodeWithCoords<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let node = self.stack.pop()?;
+        match node.node {
+            QuadTreeNode::Subdivided(children) => {
+                let side_size = node.side_size(self.top_tile_size);
+                for (i, child) in children.iter().enumerate().rev() {
+                    let x = ((i % 2) as u32 * side_size) + node.x;
+                    let y = ((i / 2) as u32 * side_size) + node.y;
+                    let child_context = QuadTreeNodeWithCoords {
+                        node: child,
+                        x,
+                        y,
+                        depth: node.depth + 1,
+                    };
+                    self.stack.push(child_context);
+                }
+                self.stack.pop()
+            }
+            _ => Some(node),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum QuadTreeNode {
+    Full,
+    Empty,
+    Subdivided(Box<[QuadTreeNode; 4]>),
+    Shaped(TileId),
+}
+
+impl QuadTreeNode {
+    fn all_white<I: GenericImageView<Pixel = Rgb<u8>>>(img: &SubImage<&I>, side_size: u32) -> bool {
+        (0..side_size)
+            .flat_map(|y| (0..side_size).map(move |x| img.get_pixel(x, y)))
+            .all(|v| v != BLACK)
+    }
+
+    fn all_black<I: GenericImageView<Pixel = Rgb<u8>>>(img: &SubImage<&I>, side_size: u32) -> bool {
+        !(0..side_size)
+            .flat_map(|y| (0..side_size).map(move |x| img.get_pixel(x, y)))
+            .any(|v| v != BLACK)
+    }
+
+    pub fn node_from_view<I: GenericImageView<Pixel = Rgb<u8>>>(
+        view: &SubImage<&I>,
+        side_size: u32,
+    ) -> Self {
+        if Self::all_white(view, side_size) {
+            Self::Full
+        } else if Self::all_black(view, side_size) {
+            Self::Empty
+        } else if side_size == SAMPLE_SIZE {
+            let mut bools = [false; (SAMPLE_SIZE * SAMPLE_SIZE) as usize];
+            for x in 0..SAMPLE_SIZE {
+                for y in 0..SAMPLE_SIZE {
+                    bools[(x + y * SAMPLE_SIZE) as usize] = view.get_pixel(x, y) != BLACK
+                }
+            }
+            Self::Shaped(TileId::from_samples(bools))
+        } else {
+            let coords = [(0, 0), (1, 0), (0, 1), (1, 1)];
+            let subdivisions = coords.map(|(x, y)| {
+                let new_side_size = side_size / 2;
+                let new_view = view.view(
+                    x * new_side_size,
+                    y * new_side_size,
+                    new_side_size,
+                    new_side_size,
+                );
+                Self::node_from_view(&new_view, new_side_size)
+            });
+            Self::Subdivided(Box::new(subdivisions))
+        }
+    }
+
+    fn get_squares(
+        &self,
+        anchor_x: u32,
+        anchor_y: u32,
+        side_size: u32,
+    ) -> Option<Vec<(u32, u32, u32)>> {
+        match self {
+            Self::Shaped(_) | Self::Empty => None,
+            Self::Full => Some(vec![(anchor_x, anchor_y, side_size)]),
+            Self::Subdivided(children) => Some(
+                children
+                    .iter()
+                    .zip([(0, 0), (1, 0), (0, 1), (1, 1)])
+                    .flat_map(|(child, (x, y))| {
+                        let new_side_size = side_size / 2;
+                        let new_anchor_x = anchor_x + (new_side_size * x);
+                        let new_anchor_y = anchor_y + (new_side_size * y);
+                        child.get_squares(new_anchor_x, new_anchor_y, new_side_size)
+                    })
+                    .flatten()
+                    .collect(),
+            ),
+        }
+    }
+
+    pub fn get_shapes(&self) -> Vec<&TileId> {
+        match self {
+            Self::Full | Self::Empty => vec![],
+            Self::Shaped(s) => vec![s],
+            Self::Subdivided(nodes) => nodes.iter().flat_map(|node| node.get_shapes()).collect(),
+        }
+    }
+
+    pub fn as_lua(&self) -> String {
+        match self {
+            Self::Empty => "0".to_string(),
+            Self::Full => "1".to_string(),
+            Self::Shaped(s) => format!("\"{s}\""),
+            Self::Subdivided(children) => {
+                let c0 = children[0].as_lua();
+                let c1 = children[1].as_lua();
+                let c2 = children[2].as_lua();
+                let c3 = children[3].as_lua();
+
+                format!("{{{c0},{c1},{c2},{c3}}}")
+            }
+        }
+    }
+}
